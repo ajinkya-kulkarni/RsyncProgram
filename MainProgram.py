d7ed22a8aff2ed66b4f592d751d17be6fa4583f3
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-27 11:10:45 +0100

    Added extra flags to delete the contents of destdirs first

--- MainProgram.py
+++ MainProgram.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Run this script as: python /home/ajinkya/Desktop/RsyncProgram/MainProgram.py
+
 import os
 import shutil
 from tqdm import tqdm
@@ -28,12 +30,18 @@ dstdir2 = "/media/ajinkya/Ajinkya/DataSSD_Backup/"
 
 # Check if the source and destination directories exist
 for srcdir, dstdir in [(srcdir1, dstdir1), (srcdir2, dstdir2)]:
-    if not os.path.isdir(srcdir):
-        print(f"Error: {srcdir} does not exist")
-    elif not os.path.isdir(dstdir):
-        print(f"Error: {dstdir} does not exist")
-    else:
-        sync_directories(srcdir, dstdir)
+	if not os.path.isdir(srcdir):
+		print(f"Error: {srcdir} does not exist")
+	elif not os.path.isdir(dstdir):
+		print(f"Error: {dstdir} does not exist")
+	else:
+		sync_directories(srcdir, dstdir)
+
+# Delete the contents of dstdir1 and dstdir2
+for dstdir in [dstdir1, dstdir2]:
+	if os.path.isdir(dstdir):
+		shutil.rmtree(dstdir)
+		os.makedirs(dstdir)
 
 ########################################################################################
 
--- modules.py
+++ modules.py
@@ -7,80 +7,80 @@ import datetime
 ########################################################################################
 
 def sync_directories(srcdir, dstdir):
-    """
-    Syncs the contents of a source directory to a destination directory. A new subdirectory is created within the
-    destination directory to store the backup of the source directory.
+	"""
+	Syncs the contents of a source directory to a destination directory. A new subdirectory is created within the
+	destination directory to store the backup of the source directory.
 
-    Parameters:
-    srcdir (str): Path to the source directory.
-    dstdir (str): Path to the destination directory.
+	Parameters:
+	srcdir (str): Path to the source directory.
+	dstdir (str): Path to the destination directory.
 
-    Returns:
-    None
+	Returns:
+	None
 
-    Raises:
-    Exception: If there is not enough space in the destination directory to copy the source directory.
-    If any errors occur during copying, an error message will be printed to the console and added to the `errors` list. 
-    At the end of the function, any errors in the `errors` list will be dumped to a text file named `Errors_<timestamp>.txt`. 
-    If no errors occur, this file will not be created.
-    """
+	Raises:
+	Exception: If there is not enough space in the destination directory to copy the source directory.
+	If any errors occur during copying, an error message will be printed to the console and added to the `errors` list. 
+	At the end of the function, any errors in the `errors` list will be dumped to a text file named `Errors_<timestamp>.txt`. 
+	If no errors occur, this file will not be created.
+	"""
 
-    errors = []  # List to store any error messages
+	errors = []  # List to store any error messages
 
-    try:
-        # Create a new directory in the destination with a timestamp
-        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        new_dstdir = os.path.join(dstdir, f"Backup_{timestamp}")
-        os.makedirs(new_dstdir, exist_ok=True)
+	try:
+		# Create a new directory in the destination with a timestamp
+		timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+		new_dstdir = os.path.join(dstdir, f"Backup_{timestamp}")
+		os.makedirs(new_dstdir, exist_ok=True)
 
-        # Check if there is enough space in the destination directory
-        available_space = shutil.disk_usage(new_dstdir).free
-        source_size = shutil.disk_usage(srcdir).used
+		# Check if there is enough space in the destination directory
+		available_space = shutil.disk_usage(new_dstdir).free
+		source_size = shutil.disk_usage(srcdir).used
 
-        if available_space < source_size:
-            raise Exception(f"Not enough space in {new_dstdir} to copy {srcdir}")
+		if available_space < source_size:
+			raise Exception(f"Not enough space in {new_dstdir} to copy {srcdir}")
 
-        # Sync the directory and display a feedback message
-        print(f"\nSyncing {srcdir} directory...\n")
+		# Sync the directory and display a feedback message
+		print(f"\nSyncing {srcdir} directory...\n")
 
-        # Use tqdm to display a progress bar
-        files_copied = 0
-        total_files = 0
+		# Use tqdm to display a progress bar
+		files_copied = 0
+		total_files = 0
 
-        for root, dirs, files in os.walk(srcdir):
-            total_files += len(files)
+		for root, dirs, files in os.walk(srcdir):
+			total_files += len(files)
 
-        with tqdm(total=total_files) as pbar:
-            for root, dirs, files in os.walk(srcdir):
-                for file in files:
-                    src_file = os.path.join(root, file)
-                    rel_path = os.path.relpath(src_file, srcdir)
-                    dst_file = os.path.join(new_dstdir, rel_path)
+		with tqdm(total=total_files) as pbar:
+			for root, dirs, files in os.walk(srcdir):
+				for file in files:
+					src_file = os.path.join(root, file)
+					rel_path = os.path.relpath(src_file, srcdir)
+					dst_file = os.path.join(new_dstdir, rel_path)
 
-                    os.makedirs(os.path.dirname(dst_file), exist_ok=True)
-                    try:
-                        shutil.copy2(src_file, dst_file)
-                    except Exception as e:
-                        error_msg = f"Error copying file {src_file}: {e}"
-                        print(error_msg)
-                        errors.append(error_msg)
+					os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+					try:
+						shutil.copy2(src_file, dst_file)
+					except Exception as e:
+						error_msg = f"Error copying file {src_file}: {e}"
+						print(error_msg)
+						errors.append(error_msg)
 
-                    files_copied += 1
-                    pbar.update(1)
+					files_copied += 1
+					pbar.update(1)
 
-        print(f"\n{srcdir} directory synced to {new_dstdir}\n")
+		print(f"\n{srcdir} directory synced to {new_dstdir}\n")
 
 
-    except Exception as e:
-        error_msg = f"Error syncing {srcdir} directory: {e}"
-        print(error_msg)
-        errors.append(error_msg)
+	except Exception as e:
+		error_msg = f"Error syncing {srcdir} directory: {e}"
+		print(error_msg)
+		errors.append(error_msg)
 
-    # Dump any error messages to a file
-    if errors:
-        error_file_name = "/home/ajinkya/Desktop/MostUsedShellScripts/Errors_{}.txt".format(timestamp)
-        with open(error_file_name, "w") as f:
-            f.write("\n".join(errors))
-        print("{} error messages written to {}".format(len(errors), error_file_name))
+	# Dump any error messages to a file
+	if errors:
+		error_file_name = "/home/ajinkya/Desktop/RsyncProgram/Errors_{}.txt".format(timestamp)
+		with open(error_file_name, "w") as f:
+			f.write("\n".join(errors))
+		print("{} error messages written to {}".format(len(errors), error_file_name))
 
 ########################################################################################
